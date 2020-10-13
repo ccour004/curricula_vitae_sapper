c77ab15c113bf3c1f0c64ec29a3aa8bb01ad7a03
Colin Courtney <ccour004@gmail.com>
2020-10-13 08:10:01 -0400

    More work on github actions.

--- src/server.js
+++ src/server.js
@@ -8,6 +8,7 @@ const dev = NODE_ENV === 'development';
 
 polka() // You can also use Express
 	.use(
+		'curricula_vitae_sapper',
 		compression({ threshold: 0 }),
 		sirv('static', { dev }),
 		sapper.middleware()
